517a235e33bc7a1bf6941994f9ef7c3b515ff1dc
beatrizpieri1 <161755243+beatrizpieri1@users.noreply.github.com>
2024-05-28 19:30:50 -0300

    js

---
+++ scrip.js
@@ -0,0 +1,27 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("carrinho-modal")
+const cartItemsConteiner = document.getElementById("items-carrinho")
+const cartTotal = document.getElementById("total-compras")
+const checkoutBtn = document.getElementById("btn-finalizar")
+const closeModal = document.getElementById("btn-fechar")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarning = document.getElementById("address-warn")
+
+cartBtn.addEventListener("click", function(){
+    cartModal.style.display = "flex"
+})
+
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModal.addEventListener("click", function(event){
+    if(event.target === closeModal){
+        cartModal.style.display = "none"
+    }
+})
+
