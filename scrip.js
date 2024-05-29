cd5da3348e935bb626ab5baa549fb57c3cdcff2d
beatrizpieri1 <161755243+beatrizpieri1@users.noreply.github.com>
2024-05-28 21:28:41 -0300

    .

--- scrip.js
+++ scrip.js
@@ -9,6 +9,8 @@ const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
 const addressWarning = document.getElementById("address-warn")
 
+let cart = []
+
 cartBtn.addEventListener("click", function(){
     cartModal.style.display = "flex"
 })
@@ -25,8 +27,39 @@ closeModal.addEventListener("click", function(event){
     }
 })
 
-checkoutBtn.addEventListener("click", function(event){
-    if(event.target === addressInput === false){
-       addressWarning.style.display = "flex"
-    } 
+checkoutBtn.addEventListener("click", function() {
+    if (addressInput.value.trim() === "") {
+        addressWarning.style.display = "flex";
+    } else {
+        addressWarning.style.display = "none";
+    }
+});
+
+
+//criando uma variavel que vai pegar o item dentro da class add-to-cart-btn
+// "parent" pega o button e o icone dentro dele
+
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton){
+        //pegando os atributos de nome e preço definidos no html
+        const name = parentButton.getAttribute("data-name")
+
+        //usando o parseFloat para aparecer como numero
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        addToCart(name, price)
+    }
 })
+
+
+// function para add no carrinho
+
+function addToCart(name, price){
+  cart.push({
+    name,
+    price,
+    quantity: 1,
+  })
+}
